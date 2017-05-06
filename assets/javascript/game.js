ff1e0f7da21159854ef218c5bb66ccbb9506eef0
Jake Wyatt <Jake Wyatt>
2017-05-06 13:58:27 -0500

    updated 050617 0158pm

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,25 @@
 var wins = 1;
 var losses = 1;
 var score = 0;
+var resetPoints = function() {
+	
 
+	var populateRandomValue = function (button) {
+		button.value = Math.floor(Math.random() * (12 - 1) + 1);
+
+	}
+
+	populateRandomValue(document.getElementById("button-1"));
+	populateRandomValue(document.getElementById("button-2"));
+	populateRandomValue(document.getElementById("button-3"));
+	populateRandomValue(document.getElementById("button-4"));
+
+};
 
 
 $(document).ready(function() {
 	var random1 = Math.floor(Math.random() * (120 - 19) + 19);
+	resetPoints();
 	
 	$("#randomNumber").text(random1);
 
@@ -14,17 +28,21 @@ $(document).ready(function() {
 		var amount = +$(this).val();
 		score = amount + score;
 		$("#score2").html(score);
+
 		if (score === random1) {
 			$("#wins").html(wins++);
 			score = 0;
 			random1 = Math.floor(Math.random() * (120 - 19) + 19);
 			$("#randomNumber").text(random1);
+			resetPoints();
 		}
 		if (score > random1) {
 			$("#losses").html(losses++);
 			score = 0;
 			random1 = Math.floor(Math.random() * (120 - 19) + 19);
 			$("#randomNumber").text(random1);
+			resetPoints();
+
 		}
 	});
 });
